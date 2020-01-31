a42914d208daf8af3e5af48ef5367cab436a69c7
Ashesh Sharma <ashesh.sharma@nrel.gov>
2020-01-31 16:51:11 -0700

    adding construction of AMG with iterative fringe update

--- src/fringe_DBC.m
+++ src/fringe_DBC.m
@@ -1,4 +1,4 @@
-function glb_res = fringe_DBC(glb_res,glb_mesh,glb_donor_map,fulldsol,glb_nd_dof_map,ov_info)
+function glb_res = fringe_DBC(glb_res,glb_jac,glb_mesh,glb_donor_map,fulldsol,glb_nd_dof_map,ov_info)
 % fringe residual contribution during iterative solve
 
 num_grids = ov_info('num grids'); % nmuber of grids
@@ -21,9 +21,47 @@ for ig = 1:num_grids
     donor_nd_dof_map = glb_nd_dof_map{donor_grid}; % extract node to dof map
    
     glb_res = fringe_contribution(ov_info,donor_map, ...
-              coords,donor_coords,nd_dof_map,donor_nd_dof_map,fulldsol, ...
-              glb_res,[],[],[],0,true);
+                                  coords,donor_coords,nd_dof_map,donor_nd_dof_map,fulldsol, ...
+                                  glb_res,[],[],[],0,true);
+          
+    glb_res = adjust_donor_rhs(ov_info,donor_map, ...
+                               coords,donor_coords,nd_dof_map,donor_nd_dof_map,fulldsol, ...
+                               glb_res,glb_jac);
+
+end
+
+end
+
+%% adjust donor rhs
+function glb_res = adjust_donor_rhs(ov_info,donor_map,coords,donor_coords,nd_dof_map,donor_nd_dof_map,dsol,glb_res,glb_jac)
+
+% loop over all fringe nodes
+for ifr = 1:size(donor_map,1)
+
+    frng_nd   = donor_map{ifr,1}; % fringe node
+    donor_nds = donor_map{ifr,2}; % extract donor node ids
+
+    frng_coords     = coords(frng_nd,:); % extract fringe node coordinates
+    donor_nd_coords = donor_coords(donor_nds,:); % extract donor node coordinates
     
+    frng_nd_dofs  = reshape(nd_dof_map(frng_nd,:)',[],1); % extract fringe node dofs
+    donor_nd_dofs = reshape(donor_nd_dof_map(donor_nds,:)',[],1); % extract donor node dofs 
+
+    % compute residual and jacobian contribution from interpolation
+    % contributions
+    coeff = compute_frg_coeff(frng_coords,donor_nd_coords,ov_info);
+    
+    % sum into global reidual
+    for r = 1:length(frng_nd_dofs)
+        
+        % jacobian contribution from d(-N*don_sol)/d(don_sol)
+        for c = 1:length(donor_nd_dofs)
+            % DBC = N*don_sol
+            glb_res(donor_nd_dofs(c)) = glb_res(donor_nd_dofs(c)) ...
+                                      + coeff'*dsol(donor_nd_dofs)*glb_jac(donor_nd_dofs(c),frng_nd_dofs(r));
+        end
+    end
+
 end
 
 end
--- src/solver_decoupled_iterative.m
+++ src/solver_decoupled_iterative.m
@@ -27,7 +27,7 @@ for io = 1:O_iters
 
     if(ov_info('fringe update') == "iterative") %
         fulldsol = iterative_fringe_update(RES,JAC_MAT,mesh_obj1,mesh_obj2,donor_map1,donor_map2, ...
-                                           nd_dof_map1,nd_dof_map2,glb_fdof,ov_info,lin_sol_info);
+                                           glb_sol_np1,nd_dof_map1,nd_dof_map2,glb_fdof,ov_info,lin_sol_info,glb_fld_dof,pp);
         
         glb_sol_np1(glb_fld_dof) = glb_sol_np1(glb_fld_dof) - fulldsol(glb_fld_dof); % update solution after linear solve
     else
@@ -49,7 +49,7 @@ for io = 1:O_iters
     % determine solution at fringe points for all meshes
     glb_sol_np1 = fringe_interpolation({mesh_obj1,mesh_obj2}, {donor_map1,donor_map2}, ...
                                         glb_sol_np1, {nd_dof_map1,nd_dof_map2}, ov_info);
-
+                                    
     L2_diff = sqrt( sum((glb_sol_np1 - glb_sol_np1_prev).^2) / numpts );
     fprintf('solution diff = %e \n\n', L2_diff);
         
@@ -68,51 +68,126 @@ end
 
 %% decoupled GMRES solve with intermediate fringe updates
 
-function fulldsol = iterative_fringe_update(RES,JAC_MAT,mesh_obj1,mesh_obj2,donor_map1,donor_map2, ...
-                                            nd_dof_map1,nd_dof_map2,glb_fdof,ov_info,lin_sol_info)
+function fulldsol = iterative_fringe_update(RHS,JAC_MAT,mesh_obj1,mesh_obj2,donor_map1,donor_map2, ...
+                                            glb_sol_np1,nd_dof_map1,nd_dof_map2,glb_fdof,ov_info,lin_sol_info,glb_fld_dof,pp)
 global gmres_iter_count; 
 
 gmres_tol   = lin_sol_info('GMRES tolerance');
 maxit       = lin_sol_info('GMRES iterations');
+U_dim       = lin_sol_info('dsol history');
 dsol_tol    = lin_sol_info('dsol tolerance');
 max_exch_it = ov_info('exchange iterations');
 
-% initialize dull change in solution array 
-fulldsol = zeros(size(RES,1));
-dsol = fulldsol(glb_fdof);
+% initialize full change in solution array 
+fulldsol = zeros(size(glb_sol_np1,1));
+free_dsol = fulldsol(glb_fdof);
+
+Q = zeros(size(glb_fdof,1), U_dim); % Orthonormal basis for the RHS subspace
+U = zeros(size(glb_fdof,1), U_dim); % Solutions satisfying A*U = Q
+R = zeros(           U_dim, U_dim); % R factor in the QR decomposition for the RHS subspace
+T = zeros(           U_dim, U_dim); % R factor in the QR decomposition for the RHS subspace
 
 % build preconditioner for JAC_MAT
-[L,U] = ilu(JAC_MAT(glb_fdof,glb_fdof),struct('type','ilutp','droptol',1e-3));
+free_JAC_MAT = JAC_MAT(glb_fdof,glb_fdof);
+[L_pre,U_pre] = ilu(free_JAC_MAT,struct('type','ilutp','droptol',1e-3));
 
-loc_count = 0; % initialize iteration counter
+U_d = 0; % initialize counter for solution history
+loc_count = 0; % initialize GMRES iteration counter
+exch_it   = 0; % initialize iteration counter
+
+glb_sol_tmp = glb_sol_np1;
+
+L2 = [];
 
 fprintf('\n');
 
-while (loc_count < max_exch_it)
+while (exch_it < max_exch_it)
 
-    dsol_prev = dsol; % update previous dsol array
+    dsol_prev = free_dsol; % update previous dsol array
     
     % determine RHS correction at fringe points for all meshes
-    RES = fringe_DBC(RES, {mesh_obj1,mesh_obj2}, {donor_map1,donor_map2}, ...
-                     fulldsol, {nd_dof_map1,nd_dof_map2}, ov_info);
+    RHS_adj = fringe_DBC(RHS, JAC_MAT, {mesh_obj1,mesh_obj2}, {donor_map1,donor_map2}, ...
+                         fulldsol, {nd_dof_map1,nd_dof_map2}, ov_info);
+    free_RES = RHS_adj(glb_fdof);
     
-    % GMRES solves
-    [dsol,~,~,~,resvec] = gmres(JAC_MAT(glb_fdof,glb_fdof),RES(glb_fdof),[], ...
-                                gmres_tol,maxit,L,U,fulldsol(glb_fdof,1));
-    loc_count = loc_count+(length(resvec)-1);
+    if(U_d == 0)    
+        % GMRES solve
+        [free_dsol,~,~,~,resvec] = gmres(free_JAC_MAT,free_RES,[],gmres_tol,maxit,L_pre,U_pre,free_dsol);
+        
+        orthg_RES = free_JAC_MAT*free_dsol;
+        
+        R(1, 1) = norm(orthg_RES);
+        Q(:, 1) = orthg_RES/R(1, 1);
+        U(:, 1) = free_dsol/R(1, 1);
+        
+    else
+        % Project onto orthogonal complement of RHSs subspace.
+        alpha = R(1:U_d,U_d);
+        RES_tilde = free_RES - Q(:,1:U_d)*alpha;
+        
+        % Component of initial solution lying in RHS subspace.
+        free_dsol_par = U(:,1:U_d)*alpha;
+        
+        % Components of initial solution perpendicular to RHS subspace.
+        free_dsol_per = U_pre\(L_pre\RES_tilde);
+        
+        init_dsol = free_dsol_par + free_dsol_per;
+        init_dsol = init_dsol + U_pre\(L_pre\(free_RES - free_JAC_MAT*init_dsol));
+        
+        % apply Hegedus scaling
+        xi_min = free_RES'*(free_JAC_MAT*init_dsol)/(norm(free_JAC_MAT*init_dsol)^2); 
+        init_dsol = xi_min*init_dsol;
+        
+        % GMRES solve
+        [free_dsol,~,~,~,resvec] = gmres(free_JAC_MAT,free_RES,[],gmres_tol,maxit,L_pre,U_pre,init_dsol);
+
+        % drop first column of Q and U and re-establish QR factorization
+        if (U_d == U_dim)
+            [Q, U, R] = dropFirstCol(Q,U,R);
+            U_d = U_d - 1;
+        end
+        
+        % CGS-2 low synch
+        orthg_RES = free_JAC_MAT*free_dsol;
+        Q(:,U_d+1) =  orthg_RES; 
+        [Q, R(1:U_d+1,1:U_d+1), T] = CGS2_twoSynch(Q(:,1:U_d+1),R(1:U_d+1,1:U_d+1),T);
+        
+        % projection and normalization of U
+        U(:,U_d+1) = free_dsol - U(:,1:U_d)*R(1:U_d,U_d+1);
+        U(:,U_d+1) = U(:,U_d+1)/R(U_d+1,U_d+1);
+
+    end
+
+    if (U_dim > 0)
+        U_d = U_d + 1; % update solution history counter
+    end
     
-    fulldsol(glb_fdof) = dsol; % update change in solution array
+    loc_count = loc_count+(length(resvec)-1); % update GMRES iteration counter
     
-    fprintf('GMRES esidual norm = %e \n', resvec(end));    
+    exch_it = exch_it + 1; % update exchange iteration counter
+    
+    fulldsol(glb_fdof) = free_dsol; % update change in solution array
+    
+    fprintf('GMRES iteration = %d \n', loc_count);    
+    fprintf('GMRES residual norm = %e \n', resvec(end));    
     if(resvec(end) < eps) % break if absolute residual is very low
         break;
     end
 
-    dsol_diff = sqrt( sum((dsol - dsol_prev).^2) / size(glb_fdof,1) );
+    dsol_diff = sqrt( sum((free_dsol - dsol_prev).^2) / size(glb_fdof,1) );
     fprintf('dsol diff = %e \n', dsol_diff);
     if(dsol_diff < dsol_tol) % check if dsol is still changing
         break
     end
+
+    glb_sol_tmp(glb_fld_dof) = glb_sol_np1(glb_fld_dof) - fulldsol(glb_fld_dof);
+
+    % determine solution at fringe points for all meshes
+    glb_sol_tmp = fringe_interpolation({mesh_obj1,mesh_obj2}, {donor_map1,donor_map2}, ...
+                                        glb_sol_tmp, {nd_dof_map1,nd_dof_map2}, ov_info);
+
+    L2 = [L2, comp_L2_err(pp,glb_sol_tmp,mesh_obj1,mesh_obj2,0)];
+    fprintf('\n\n');
     
 end
 
@@ -121,6 +196,50 @@ fprintf('Total GMRES iterations = %d \n\n', gmres_iter_count);
 
 end
  
+% Drop the first column of the QR factorization, zeroing the last columns of Q
+% and R.
+function [Q, U, R] = dropFirstCol(Q, U, R)
 
+dim = size(Q, 2);
 
+R = R(:, 2:end);
 
+for i = 1:1:(dim - 1)
+    G = planerot(R(i:(i + 1), i));
+    R(i:(i + 1), :) = G*R(i:(i + 1), :);
+    
+    Q(:, i:(i + 1)) = Q(:, i:(i + 1))*G';
+    
+    U(:, i:(i + 1)) = U(:, i:(i + 1))*G';
+end
+
+Q(:, dim) = 0;
+U(:, dim) = 0;
+
+R = [R zeros(dim, 1)];
+end
+
+% Classical Gram Schmidt algorithm  
+function [Q, R, T] = CGS2_twoSynch(Q, R, T)
+
+j = size(Q,2);
+
+if (j>1)     
+        tmp = Q(:, 1:j-1)'*Q(:, j-1:j);
+        T(1:j-1, j-1)  = tmp(:,1);
+        a = tmp(:,2);
+        
+        L(1:j-1, 1:j-1) = tril(T(1:j-1, 1:j-1),-1);
+        
+        R(1:j-1, j) = (eye(j-1,j-1) - L(:, 1:j-1) - L(:, 1:j-1)')*a;
+        
+        Q(:,j) = Q(:,j) - Q(:, 1:j-1)*R(1:j-1,j);
+ 
+        Q(:,j) = Q(:,j) - Q(:,1:j-1)*(Q(:,1:j-1)'*Q(:,j));
+             
+end
+
+R(j, j) = norm(Q(:,j));
+Q(:, j) =  Q(:, j)/norm(Q(:,j));
+
+end
